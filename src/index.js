097ee2975bfbe6f06f9699b3db2b6152e8c7cd0b
westbro <westbro00@gmail.com>
2018-10-25 12:14:04 +0900

    JS: add baseball logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
-});
+let tryNum = [4, 5, 6];
+const answer = [6, 6, 3];
+const tryNumSize = tryNum.length;
+
+function oneGame() {
+  if (isHomeRun(tryNum, answer)) {
+    console.log("즈엉엉답ㄷ");
+  } else {
+    const strike = howManyStrike(tryNum, answer);
+    const ball = howManyBall(tryNum, answer);
+    console.log(strike + " strike");
+    console.log(ball + " ball");
+    if (strike === 0 && ball === 0) {
+      console.log('아우우웃~!@');
+    }
+  }
+}
+
+function isHomeRun() {
+  let homerun = 0;
+  for (let i = 0; i < tryNumSize; i++) {
+    if (tryNum[i] === answer[i]) {
+      homerun++;
+    }
+  }
+  if (homerun === tryNumSize) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function howManyStrike() {
+  let strike = 0;
+  for (let i = 0; i < tryNumSize; i++) {
+    if (tryNum[i] === answer[i]) {
+      strike++;
+      tryNum[i] = NaN;
+    }
+  }
+  return strike;
+}
+
+function howManyBall() {
+  let ball = 0;
+  for (let i = 0; i < tryNumSize; i++) {
+    for (let j = 0; j < tryNumSize; j++) {
+      if (tryNum[i] === answer[j]) {
+        ball++;
+        break;
+      }
+    }
+  }
+  return ball;
+}
+
+oneGame();
